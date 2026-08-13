b96786914639e2f1b115e0b0bb0b1bbb94ba69f8
RoyV5 <rodrigoolmospercoco@gmail.com>
2026-08-13 19:51:31 -0400

    Minor bug fix in navigation to register screen

--- src/navigation/AuthNavigator.jsx
+++ src/navigation/AuthNavigator.jsx
@@ -1,33 +1,22 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { useAuth } from '../context/AuthContext';
 import LoginScreen from '../screens/auth/LoginScreen';
 import RegisterScreen from '../screens/auth/RegisterScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function AuthNavigator() {
-    const { login } = useAuth();
-
     return (
         <Stack.Navigator>
-            <Stack.Screen name="Login">
-                {({ navigation }) => (
-                    <LoginScreen
-                        onLogin={login}
-                        onRegister={() => navigation.navigate('Register')}
-                    />
-                )}
-            </Stack.Screen>
+            <Stack.Screen
+                name="Login"
+                component={LoginScreen}
+            />
 
-            <Stack.Screen name="Register">
-                {({ navigation }) => (
-                    <RegisterScreen
-                        onRegister={login}
-                        onBack={() => navigation.goBack()}
-                    />
-                )}
-            </Stack.Screen>
+            <Stack.Screen
+                name="Register"
+                component={RegisterScreen}
+            />
         </Stack.Navigator>
     );
 }
